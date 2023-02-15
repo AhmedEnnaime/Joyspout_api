700c5e0aff3d681b479816f239624576604b7ca2
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-02-15 12:17:51 +0100

    feat: :rocket: setting up the models and controllers

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
+
+class CategoryController extends BaseController
+{
+    //
+}
---
+++ app/Http/Controllers/API/MediaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+
+class MediaController extends BaseController
+{
+    //
+}
